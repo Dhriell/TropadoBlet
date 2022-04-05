605ddc1b691863b2e875a276129f4ab391c89d34
Dhriell <86806291+Dhriell@users.noreply.github.com>
2022-04-05 17:40:56 -0300

    Atualização de imagens

---
+++ assets/js/content.js
@@ -0,0 +1,160 @@
+let users = [
+    {   
+        id: 0,
+        nick: 'Dhriell',
+        color: '#0085FF',
+        shadowText: '0px 4px 10px rgba(11, 127, 236, 0.3)',
+        data: '15/07/2017',
+        descricao: '021 - abre.ai/b91g',
+        personalidade: 'Consquistador nato, carioca otaku fedido, jodador mais estiloso do habblet ',
+        apelido: 'BRABO, EXTERMINADOR DE OTÁRIOS, MAGO IMPLACÁVEL',
+        imagem: 'assets/img/pessoas/dhriell.png',
+        quartos:[
+            {quarto:'assets/img/quartos/dhriell/1.png'},
+            {quarto:'assets/img/quartos/dhriell/2.png'},
+            {quarto:'assets/img/quartos/dhriell/3.png'},
+            {quarto:'assets/img/quartos/dhriell/4.png'}
+        ],
+        background: 'assets/img/quartos/dhriell/background/fundo.png'
+
+
+    },
+
+    {   
+        id: 1,
+        nick: 'PinkZeroK',
+        color: '#ff0000',
+        shadowText: '0px 4px 10px rgba(255, 0, 0, 0.2)',
+        data: '31/05/2019',
+        descricao: '...',
+        personalidade: 'faz muitos gados para ganhar dinheiro em jogo, sabe como controlar a situação dos gados',
+        apelido: 'loli, egirl, rainha do gado',
+        imagem: 'assets/img/pessoas/pinkzerok.png',
+        quartos:[
+            {quarto:'assets/img/quartos/pinkzerok/1.png'},
+            {quarto:'assets/img/quartos/pinkzerok/2.png'},
+            {quarto:'assets/img/quartos/pinkzerok/3.png'},
+            {quarto:'assets/img/quartos/pinkzerok/4.png'}
+        ],
+        background: 'assets/img/quartos/pinkzerok/background/fundo.png'
+    },
+
+    {   
+        id: 2,
+        nick: 'Architecting',
+        color: '#ff8800',
+        shadowText: '0px 4px 10px rgba(253, 173, 1, 0.2)',
+        data: '11/11/2021',
+        descricao: 'mago dos magos',
+        personalidade: 'maior pegador de casada do jogo, rei delas',
+        apelido: 'anão de jardim, calvo junior, webpegador master',
+        imagem: 'assets/img/pessoas/architecting.png',
+        quartos:[
+            {quarto:'assets/img/quartos/architecting/1.png'},
+            {quarto:'assets/img/quartos/architecting/2.png'},
+        ],
+        background: 'assets/img/quartos/architecting/background/fundo.png'
+    },
+
+    {   
+        id: 3,
+        nick: 'Allan11',
+        color: '#00d8f5',
+        shadowText: '0px 4px 10px rgba(0, 255, 242, 0.2)',
+        data: '17/05/2021',
+        descricao: 'sem descrição pq é mané',
+        personalidade: 'calvo supremo ladrão de quartos alheios',
+        apelido: 'calvo supremo, usa batom na foto',
+        imagem: 'assets/img/pessoas/allan11.png',
+        quartos:[
+        ],
+        background: 'assets/img/quartos/allan11/background/fundo.png'
+    },
+
+    {   
+        id: 4,
+        nick: 'Riqueea',
+        color: '#995200',
+        shadowText: '0px 4px 10px rgba(165, 60, 0, 0.2)',
+        data: '19/03/2020',
+        descricao: 'abre.ai/b91g',
+        personalidade: 'aleatorio aleatorio teste aleatorio aleatorio teste',
+        apelido: 'aleatorio aleatorio teste aleatorio aleatorio teste',
+        imagem: 'assets/img/pessoas/riqueea.png',
+        quartos:[
+            {quarto:'assets/img/quartos/riqueea/1.png'},
+            {quarto:'assets/img/quartos/riqueea/2.png'},
+        ],
+        background: 'assets/img/quartos/riqueea/background/fundo.jpg'
+    },
+
+    {   
+        id: 5,
+        nick: 'BundaMole01',
+        color: '#8b16eb',
+        shadowText: '0px 4px 10px rgba(139, 12, 243, 0.7)',
+        data: '24/03/2020',
+        descricao: 'sem foto pq sou feio',
+        personalidade: 'aleatorio aleatorio teste aleatorio aleatorio teste',
+        apelido: 'aleatorio aleatorio teste aleatorio aleatorio teste',
+        imagem: 'assets/img/pessoas/molesbunda.png',
+        quartos:[
+            {quarto:'assets/img/quartos/molesbunda/1.png'},
+            {quarto:'assets/img/quartos/molesbunda/2.png'},
+        ],
+        background: 'assets/img/quartos/molesbunda/background/fundo.png'
+    },
+
+    {   
+        id: 6,
+        nick: 'Yawkii',
+        color: '#ff13b0',
+        shadowText: '0px 4px 10px rgba(235, 23, 224, 0.2)',
+        data: '10/04/2017',
+        descricao: "humans don't exist . abre.ai/yawkiii .",
+        personalidade: 'aleatorio aleatorio teste aleatorio aleatorio teste',
+        apelido: 'aleatorio aleatorio teste aleatorio aleatorio teste',
+        imagem: 'assets/img/pessoas/yawkii.png',
+        quartos:[
+            {quarto:'assets/img/quartos/yawkii/1.png'},
+            {quarto:'assets/img/quartos/yawkii/2.png'},
+            {quarto:'assets/img/quartos/yawkii/3.png'},
+        ],
+        background: 'assets/img/quartos/yawkii/background/fundo.png'
+    },
+
+    {   
+        id: 7,
+        nick: 'Morganaamb',
+        color: '#08f828',
+        shadowText: '0px 4px 10px rgba(5, 255, 5, 0.2)',
+        data: '07/07/2021',
+        descricao: "existindo",
+        personalidade: 'aleatorio aleatorio teste aleatorio aleatorio teste',
+        apelido: 'aleatorio aleatorio teste aleatorio aleatorio teste',
+        imagem: 'assets/img/pessoas/morgana.png',
+        quartos:[
+            {quarto:'assets/img/quartos/morgana/1.png'},
+        ],
+        background: 'assets/img/quartos/morgana/background/fundo.png'
+    },
+
+    {   
+        id: 8,
+        nick: 'Dyss',
+        color: '#ffee00',
+        shadowText: '0px 4px 10px rgba(251, 255, 1, 0.2',
+        data: '28/11/2021',
+        descricao: 'Estude.',
+        personalidade: 'Adora conversar, estressada demais, some do nada, muito sentimental',
+        apelido: 'Disy, Vanna, Gi, Elfa',
+        imagem: 'assets/img/pessoas/dyss.png',
+        quartos:[
+            {quarto:'assets/img/quartos/dyss/1.png'},
+            {quarto:'assets/img/quartos/dyss/2.png'},
+            {quarto:'assets/img/quartos/dyss/3.png'},
+        ],
+        background: 'assets/img/quartos/dyss/background/fundo.png'
+    },
+];
+
